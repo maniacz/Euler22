2a5d389fa522a5c88cae99fe8e5ff326df13bba8
Łukasz Kipka <maniacz@interia.eu>
2015-12-23 13:04:27 +0100

    Fianal working

--- Program.cs
+++ Program.cs
@@ -8,8 +8,66 @@ namespace Euler22
 {
     class Program
     {
+        private static List<string> Names;
+
         static void Main(string[] args)
         {
+            Names = createNamesArray(readFile());
+            sortNames(Names);
+
+            Console.WriteLine(findFinalResult(Names));
+            Console.ReadKey();
+        }
+
+        private static string readFile()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "\\p022_names.txt";
+            string fileContent = System.IO.File.ReadAllText(path);
+
+            return fileContent;
+        }
+
+        /// <summary>
+        /// Tworzy tablicę stringów z nazwami
+        /// </summary>
+        private static List<String> createNamesArray(string fileText)
+        {
+            string content = fileText.Replace("\"", "");
+            string[] names = content.Split(',');
+
+            return new List<String>(names);
+        }
+
+        private static void sortNames(List<string> namesToSort)
+        {
+            namesToSort.Sort();
+        }
+
+        private static int calculateNameScore(string name)
+        {
+            int score = 0;
+
+            foreach(char c in name) 
+            {
+                score += c - 64;
+            }
+
+            return score;
+        }
+
+        private static ulong findFinalResult(List<string> names)
+        {
+            int nameIndex, nameValue;
+            ulong finalResult = 0;
+
+            foreach (string name in names)
+            {
+                nameIndex = names.IndexOf(name) + 1;
+                nameValue = nameIndex * calculateNameScore(name);
+                finalResult += (ulong)nameValue;
+            }
+
+            return finalResult;
         }
     }
 }
